632ac36db61b19d424fe0f961bbf171b3d323095
Lukas <80986503+LukasGallasch@users.noreply.github.com>
2022-05-12 20:02:22 +0200

    Update youtube_scraper.py

--- youtube_scraper.py
+++ youtube_scraper.py
@@ -108,29 +108,6 @@ class YT_stats:
 
         return channel_videos, nextPageToken
 
-    def extract_videos(self):
-        channel_id = "UCm3_j4RLEzgMovQTRPx47MQ"
-        db = DB(self.db)
-        x = open("results/drachen_lord.json", mode="r")
-        vids = json.load(x)
-
-        c = 0
-        for vid in vids[channel_id]["video_data"].values():
-
-            vid_id = list(vids[channel_id]["video_data"].keys())[c]
-            title = vid["title"]
-            date = datetime.datetime.strptime(vid["publishedAt"], "%Y-%m-%dT%H:%M:%S%z")
-            likes = vid["likeCount"]
-            views = vid["viewCount"]
-            try:
-                comment_int = vid["commentCount"]
-            except KeyError:
-                comment_int = "NULL"
-            sql = "INSERT INTO `videos` (`video_id`, `publishedAt`, `title`, `viewCount`, `likeCount`, `commentCount`) VALUES (%s, %s, %s, %s, %s, %s)"
-            val = vid_id, date, title, views, likes, comment_int
-            db.db_insert(sql, val)
-            print(val)
-            c += 1
 
 class YT_Comments:
     def __init__(self, api_key, db=None):
